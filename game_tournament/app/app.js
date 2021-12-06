35b043e6822d07f5de9870f59e3ee3d88c287639
Dhruvesh3012 <dhruvesh207@gmail.com>
2021-12-06 14:07:55 +0000

    add new scoreboard.js file

--- game_tournament/app/app.js
+++ game_tournament/app/app.js
@@ -1,5 +1,6 @@
 // Import express.js
 const express = require("express");
+//const { Scoreboard } = require("./models/scoreboard");
 
 // Create express app
 var app = express();
@@ -9,6 +10,43 @@ app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+
+// Create a route for root - /
+app.get("/", function(req, res) {
+    res.send("Hello World");
+});
+
+// Get the models
+const { Scoreboard } = require("./models/scoreboard");
+
+
+
+// Create a route for testing the databse of games
+app.get("/Point_Table", function(req, res) {
+    var scoreboard = new Scoreboard();
+    scoreboard.getScoreboard().then( 
+        Promise => {
+        res.send(scoreboard);
+    });
+
+});
+
+// Create a route for testing the databse of games
+app.get("/point", function(req, res) {
+    // Assumes a table called test_table exists in your database
+    sql = 'select * from tournamentleaderboard';
+    db.query(sql).then(results => {
+        console.log(results);
+        res.send(results)
+    });
+});
+
+
+// Get the models
+//const { Scoreboard } = require("./models/scoreboard");
+
+
+
 
 // Start server on port 3000
 app.listen(3000,function(){
