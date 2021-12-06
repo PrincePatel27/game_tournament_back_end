b671a2ed261b03fbe60fc8ce5498e51435e78f43
PrincePatel27 <patelprince2797@gmail.com>
2021-12-06 14:12:09 +0000

    modify app.js file

--- game_tournament/app/app.js
+++ game_tournament/app/app.js
@@ -10,6 +10,7 @@ app.use(express.static("static"));
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
+
 // Start server on port 3000
 app.listen(3000,function(){
     console.log(`Server running at http://127.0.0.1:3000/`);
